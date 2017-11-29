0fd1fb543da3144a02ff5c4682538fcc0dabdb0a
Guillaume Chau <guillaume.b.chau@gmail.com>
2017-11-29 13:28:08 +0100

    Demo update

--- demo-src/src/PageC.vue
+++ demo-src/src/PageC.vue
@@ -2,6 +2,8 @@
   <div class="page">
     <h1>Nested routes</h1>
 
+    <SubComp />
+
     <nav>
       <router-link :to="{ name: 'page-c1' }" exact>Nested 1</router-link>
       <router-link :to="{ name: 'page-c2' }" exact>Nested 2</router-link>
@@ -10,3 +12,29 @@
     <router-view />
   </div>
 </template>
+
+<script>
+import SubComp from './PageCSub.vue'
+
+export default {
+  components: {
+    SubComp,
+  },
+
+  data () {
+    return {
+      foo: {
+        bar: ':)',
+      },
+    }
+  },
+
+  activated () {
+    console.log('PageC activated', this.foo.bar)
+  },
+
+  deactivated () {
+    console.log('PageC deactivated', this.foo.bar)
+  },
+}
+</script>
---
+++ demo-src/src/PageCSub.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>Sub component</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      foo: {
+        bar: 42,
+      },
+    }
+  },
+
+  activated () {
+    console.log('PageCSub activated', this.foo.bar)
+  },
+
+  deactivated () {
+    console.log('PageCSub deactivated', this.foo.bar)
+  },
+}
+</script>
