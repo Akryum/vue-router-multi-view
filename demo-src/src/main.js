42b1478ec395b4e2e63e1e365972d82631e1389e
ysKim <znxkznxk1030@gmail.com>
2019-03-07 10:52:19 +0900

    demo: allow devtools

--- demo-src/src/main.js
+++ demo-src/src/main.js
@@ -3,6 +3,8 @@ import { router } from './router'
 import VueRouterMultiView from '../../'
 import App from './App.vue'
 
+Vue.config.devtools = true;
+
 Vue.use(VueRouterMultiView)
 
 // eslint-disable-next-line no-new
